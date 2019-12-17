08a1e4cec073fd24ccf0ac64af0adb2098075dc8
William <williamsouef@gmail.com>
2019-12-17 12:15:54 +0100

    initialcommit

---
+++ Calculator/git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by William Souef on 02/12/2019.
+//  Copyright © 2019 William Souef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
